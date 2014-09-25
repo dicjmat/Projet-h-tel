29f491cada721ef2f22845523c03257d32d306d5
dicjmat <marc-ant1@hotmail.com>
2014-09-25 13:53:14 -0400

    login amélioré

--- GestionHotel/iLogin.xaml.vb
+++ GestionHotel/iLogin.xaml.vb
@@ -1,6 +1,8 @@
 ﻿Public Class iLogin
     Dim maBD As P2014_Equipe2_GestionHôtelièreEntities
-
+    Dim checkList As iCheckList
+    Dim gerant As iAccueilGerant
+    Dim gestion As iAccueilGestionnaire
     Private Sub btnQuitter_Click(sender As Object, e As RoutedEventArgs) Handles btnQuitter.Click
         Me.Close()
     End Sub
@@ -11,16 +13,32 @@
 
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         maBD = New P2014_Equipe2_GestionHôtelièreEntities
+        checkList = New iCheckList
+        gerant = New iAccueilGerant
+        gestion = New iAccueilGestionnaire
         txtNomUtilisateur.Focus()
     End Sub
 
     Private Sub login()
+        Dim typeEmploye As String
         Dim res = From el In maBD.tblLogin Where el.utilisateur = txtNomUtilisateur.Text And txtMDP.Password = el.mdp Select el
         If Not My.Computer.Network.IsAvailable Then
             lblErreur.Content = "Erreur de connexion"
         ElseIf res.ToList().Count = 0 Then
             lblErreur.Content = "Le nom d'utilisateur ou le mot de passe est invalide"
         End If
+        typeEmploye = res.First.statut
+        Select Case typeEmploye
+            Case "ADMI"
+                gerant.Show()
+            Case "PERS"
+                checkList.Show()
+            Case "GEST"
+                gestion.Show()
+            Case Else
+
+        End Select
+
     End Sub
 
     Private Sub Window_KeyDown(sender As Object, e As KeyEventArgs)
