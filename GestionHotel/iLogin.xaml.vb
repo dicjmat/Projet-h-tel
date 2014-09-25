4125da0f258903ba79b092bf13e412ecaaf1793f
dicjmat <marc-ant1@hotmail.com>
2014-09-25 09:29:57 -0400

    login code

--- GestionHotel/iLogin.xaml.vb
+++ GestionHotel/iLogin.xaml.vb
@@ -6,17 +6,26 @@
     End Sub
 
     Private Sub btnConfirmer_Click(sender As Object, e As RoutedEventArgs) Handles btnConfirmer.Click
-        Dim res = From el In maBD.tblLogin Where el.utilisateur = txtNomUtilisateur.Text And txtMDP.Password = el.mdp Select el
-        'If Not My.Computer.Network.IsAvailable Then
-        '    lblReussi.Content = "Erreur de connexion"
-        'ElseIf res.ToList().Count = 0 Then
-        '    lblReussi.Content = "Erreur de connexion"
-        'Else
-        '    lblReussi.Content = "Connexion réussie"
-        'End If
+        login()
     End Sub
 
-    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+    Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         maBD = New P2014_Equipe2_GestionHôtelièreEntities
+        txtNomUtilisateur.Focus()
+    End Sub
+
+    Private Sub login()
+        Dim res = From el In maBD.tblLogin Where el.utilisateur = txtNomUtilisateur.Text And txtMDP.Password = el.mdp Select el
+        If Not My.Computer.Network.IsAvailable Then
+            lblErreur.Content = "Erreur de connexion"
+        ElseIf res.ToList().Count = 0 Then
+            lblErreur.Content = "Le nom d'utilisateur ou le mot de passe est invalide"
+        End If
+    End Sub
+
+    Private Sub Window_KeyDown(sender As Object, e As KeyEventArgs)
+        If e.Key = Key.Enter Then
+            login()
+        End If
     End Sub
 End Class
