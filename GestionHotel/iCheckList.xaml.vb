5d0c0b1d4f7977c56f56bdaf10eafa4c953a6f76
dicjDL <domtrio@hotmail.com>
2014-09-26 13:44:03 -0400

    tiens marc

--- GestionHotel/iCheckList.xaml.vb
+++ GestionHotel/iCheckList.xaml.vb
@@ -1,27 +1,35 @@
 ﻿Public Class iCheckList
 
-    Private _p1 As String
+    Private _p1 As Short
+    Private BD As P2014_Equipe2_GestionHôtelièreEntities
+    Private hotel
 
-    Sub New(p1 As String)
-        ' TODO: Complete member initialization 
+    Sub New(p1 As Short)
         InitializeComponent()
         _p1 = p1
     End Sub
 
-    Dim BD As P2014_Equipe2_GestionHôtelièreEntities
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
         Me.Owner.Show()
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
         BD = New P2014_Equipe2_GestionHôtelièreEntities
-
-        Dim res = From el In BD.tblChambre Select el
+        hotel = From el In BD.tblEmploye Where el.noEmpl = _p1 Select el.noHotel
+        Dim res = From el In BD.tblChambre Where el.noHotel = hotel.FirstOrDefault() Select el
 
         cbChambre.DataContext = res.ToList()
     End Sub
 
     Private Sub btnQuitter_Click(sender As Object, e As RoutedEventArgs) Handles btnQuitter.Click
         Me.Close()
+    End Sub
+
+    Private Sub cbChambre_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cbChambre.SelectionChanged
+        Dim chambre = Convert.ToInt16(cbChambre.SelectedValue.nom())
+        Dim typeChambre = From el In BD.tblChambre Where el.noChambre = chambre And el.noHotel =
+        Dim res = From el In BD.tblGabarit Where (el.nom = chambre) Select el
+
+        lbTest.DataContext = res.ToList()
     End Sub
 End Class
