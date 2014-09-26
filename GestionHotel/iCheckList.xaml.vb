a20f3731583359b5baefc4374b12fee4e06b8457
dicjDL <domtrio@hotmail.com>
2014-09-26 15:15:22 -0400

    CheckList

--- GestionHotel/iCheckList.xaml.vb
+++ GestionHotel/iCheckList.xaml.vb
@@ -1,14 +1,13 @@
 ﻿Public Class iCheckList
 
-    Private _p1 As Short
-    Private _p2 As Short
+    Private noEmploye As Short
+    Private noHotel As Short
     Private BD As P2014_Equipe2_GestionHôtelièreEntities
-    Private hotel
 
-    Sub New(p1 As Short, p2 As Short)
+    Sub New(_noEmploye As Short, _noHotel As Short)
         InitializeComponent()
-        _p1 = p1
-        _p2 = p2
+        noEmploye = _noEmploye
+        noHotel = _noHotel
     End Sub
 
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
@@ -16,11 +15,8 @@
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        Dim nHotel As Short
         BD = New P2014_Equipe2_GestionHôtelièreEntities
-        hotel = From el In BD.tblEmploye Where el.noEmpl = _p1 Select el.noHotel
-        nHotel = Convert.ToInt16(hotel.First())
-        Dim res = From el In BD.tblChambre Where el.noHotel = nHotel Select el
+        Dim res = From el In BD.tblChambre Where el.noHotel = noHotel Select el
 
         cbChambre.DataContext = res.ToList()
     End Sub
@@ -30,9 +26,10 @@
     End Sub
 
     Private Sub cbChambre_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cbChambre.SelectionChanged
-        Dim chambre = Convert.ToInt16(cbChambre.SelectedValue.nom())
-        Dim typeChambre = From el In BD.tblChambre Where el.noChambre = chambre And el.noHotel = Convert.ToInt16(hotel) Select el.codeTypeChambre
-        Dim res = From el In BD.tblItem Join comp In BD.tblGabarit On comp.codeItem Equals el.codeItem Where comp.codeTypeChambre = typeChambre.ToString() Select el.descItem
+        Dim chambre = Convert.ToInt16(cbChambre.SelectedValue.noChambre())
+        Dim typeChambre = From el In BD.tblChambre Where el.noChambre = chambre And el.noHotel = noHotel Select el.codeTypeChambre
+        Dim codeType = typeChambre.First.ToString()
+        Dim res = From el In BD.tblItem Join comp In BD.tblGabarit On comp.codeItem Equals el.codeItem Where comp.codeTypeChambre = codeType Select el
 
         lbItem.DataContext = res.ToList()
     End Sub
