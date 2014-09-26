82b58c07f5965acb209289ce833a75fc7b6cfda7
dicjDL <domtrio@hotmail.com>
2014-09-26 10:25:21 -0400

    oui marc

--- GestionHotel/iCheckList.xaml.vb
+++ GestionHotel/iCheckList.xaml.vb
@@ -1,6 +1,9 @@
 ﻿Public Class iCheckList
-
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
         Me.Owner.Show()
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
     End Sub
 End Class
