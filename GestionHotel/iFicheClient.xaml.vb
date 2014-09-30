c46c7dc0f79442b0ddfe17ab493af95dd70d355a
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-09-30 15:38:09 -0400

    COMITT LOL

--- GestionHotel/iFicheClient.xaml.vb
+++ GestionHotel/iFicheClient.xaml.vb
@@ -1,7 +1,12 @@
 ﻿Public Class iFicheClient
     Dim db As P2014_Equipe2_GestionHôtelièreEntities
-    Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
+    Private Sub windowFicheCli_Loaded(sender As Object, e As RoutedEventArgs) Handles windowFicheCli.Loaded
         db = New P2014_Equipe2_GestionHôtelièreEntities
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        'Sauvegarder
+
     End Sub
 
 End Class
