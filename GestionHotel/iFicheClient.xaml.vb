78def21c7b9b710021d10f2eae32851dc017641e
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-09-30 16:12:14 -0400

    sdf

--- GestionHotel/iFicheClient.xaml.vb
+++ GestionHotel/iFicheClient.xaml.vb
@@ -18,4 +18,12 @@
         db.SaveChanges()
     End Sub
 
+    Private Sub btnLierClient_Click(sender As Object, e As RoutedEventArgs) Handles btnLierClient.Click
+
+    End Sub
+
+    Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
+        Me.Close()
+    End Sub
+
 End Class
