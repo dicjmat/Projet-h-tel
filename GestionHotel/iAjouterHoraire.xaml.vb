0f6b9b298fe5572de739494d3527bebda9b986eb
dicjDL <domtrio@hotmail.com>
2014-10-02 10:04:26 -0400

    ok

--- GestionHotel/iAjouterHoraire.xaml.vb
+++ GestionHotel/iAjouterHoraire.xaml.vb
@@ -1,5 +1,9 @@
 ﻿Public Class iAjouterHoraire
     Dim bd As P2014_Equipe2_GestionHôtelièreEntities
+    Sub New(_noEmploye As Short)
+        InitializeComponent()
+        'noEmploye = _noEmploye
+    End Sub
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
         bd = New P2014_Equipe2_GestionHôtelièreEntities
 
