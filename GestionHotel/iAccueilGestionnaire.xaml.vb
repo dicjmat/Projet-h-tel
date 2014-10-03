bd88cbc8fa8304eec73bc3f0fb20dfea2f2d5cf7
David Savard <david.savard18@hotmail.com>
2014-10-03 14:50:08 -0400

    bouton accueil gest

--- GestionHotel/iAccueilGestionnaire.xaml.vb
+++ GestionHotel/iAccueilGestionnaire.xaml.vb
@@ -10,22 +10,10 @@
         noEmpl = p1
         noHotel = p2
     End Sub
-    Private Sub btnInventaire_Click(sender As Object, e As RoutedEventArgs) Handles btnInventaire.Click
-        Dim inventaire = New iInventaire(noHotel)
-        inventaire.Owner = Me
-        inventaire.Show()
-    End Sub
 
     Private Sub btnDeco_Click(sender As Object, e As RoutedEventArgs) Handles btnDeco.Click
         Me.Owner.Show()
         Me.Close()
-    End Sub
-
-    Private Sub btnListeEmploye_Click(sender As Object, e As RoutedEventArgs) Handles btnListeEmploye.Click
-        Dim iEmploye = New iListeEmploye(noEmpl, noHotel)
-        iEmploye.Owner = Me
-        iEmploye.Show()
-        Me.Hide()
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
@@ -35,10 +23,23 @@
         lblNom.Content = "Bonjour, " + res.ToList.Single.prenEmpl + " " + res.ToList.Single.nomEmpl
     End Sub
 
-    Private Sub btnListeCommande_Click(sender As Object, e As RoutedEventArgs) Handles btnListeCommande.Click
+    Private Sub AppuieItem()
+        Dim inventaire = New iInventaire(noHotel)
+        inventaire.Owner = Me
+        inventaire.Show()
+    End Sub
+
+    Private Sub AppuieCommande()
         Dim lstcommande = New iListeCOmmande()
         lstcommande.Owner = Me
         Me.Hide()
         lstcommande.Show()
+    End Sub
+
+    Private Sub AppuieEmploye()
+        Dim iEmploye = New iListeEmploye(noEmpl, noHotel)
+        iEmploye.Owner = Me
+        iEmploye.Show()
+        Me.Hide()
     End Sub
 End Class
