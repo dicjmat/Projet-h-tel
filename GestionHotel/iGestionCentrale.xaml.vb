5f30e70ddb4aebd955718d1b737fc22c7cab9034
David Savard <david.savard18@hotmail.com>
2014-10-05 16:12:19 -0400

    un peu coreection

--- GestionHotel/iGestionCentrale.xaml.vb
+++ GestionHotel/iGestionCentrale.xaml.vb
@@ -1,8 +1,3 @@
 ﻿Public Class iGestionCentrale
 
-    Private Sub btnAjoutForf_Click(sender As Object, e As RoutedEventArgs) Handles btnAjoutForf.Click
-        Dim ajoutForf As iAjoutForf
-        ajoutForf = New iAjoutForf
-        ajoutForf.Show()
-    End Sub
 End Class
