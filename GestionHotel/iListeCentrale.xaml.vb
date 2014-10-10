afed953292efb370e195807eb78c254f8ff93260
David Savard <david.savard18@hotmail.com>
2014-10-10 10:02:39 -0400

    ajout

--- GestionHotel/iListeCentrale.xaml.vb
+++ GestionHotel/iListeCentrale.xaml.vb
@@ -1,6 +1,25 @@
 ﻿Public Class iListeCentrale
 
     Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
+        Me.Owner.Show()
         Me.Close()
+    End Sub
+
+    Private Sub btnAjouterProv_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouterProv.Click
+        Dim prov = New AjoutProvince
+        prov.Owner = Me
+        prov.ShowDialog()
+    End Sub
+
+    Private Sub btnAjouterVille_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouterVille.Click
+        Dim ville = New AjoutVille
+        ville.Owner = Me
+        ville.ShowDialog()
+    End Sub
+
+    Private Sub btnAjouterPays_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouterPays.Click
+        Dim pays = New AjoutPays
+        pays.Owner = Me
+        pays.ShowDialog()
     End Sub
 End Class
