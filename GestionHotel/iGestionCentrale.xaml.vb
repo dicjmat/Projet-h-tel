8b443197430dedf3979bb84a7958518a98b3f294
David Savard <david.savard18@hotmail.com>
2014-10-10 11:58:45 -0400

    accueil centrale

--- GestionHotel/iGestionCentrale.xaml.vb
+++ GestionHotel/iGestionCentrale.xaml.vb
@@ -1,10 +1,24 @@
 ﻿Public Class iGestionCentrale
+    Private Sub AppuieRapport()
 
-    Private Sub btnListeCentrale_Click(sender As Object, e As RoutedEventArgs) Handles btnListeCentrale.Click
+    End Sub
+    Private Sub AppuielstCent()
         Dim liste = New iListeCentrale()
         liste.Owner = Me
         liste.Show()
         Me.Hide()
+    End Sub
+
+    Private Sub AppuielstEmp()
+        Dim lstemp = New iListeEmployeComplet()
+        lstemp.Owner = Me
+        lstemp.Show()
+    End Sub
+
+    Private Sub AppuieInvComp()
+        Dim invComplet = New iInventaireComplet()
+        invComplet.Owner = Me
+        invComplet.Show()
     End Sub
 
     Private Sub btnDeco_Click(sender As Object, e As RoutedEventArgs) Handles btnDeco.Click
--- GestionHotel/iListeEmployeComplet.xaml.vb
+++ GestionHotel/iListeEmployeComplet.xaml.vb
@@ -1,3 +1,13 @@
 ﻿Public Class iListeEmployeComplet
 
+    Private Sub btnAjout_Click(sender As Object, e As RoutedEventArgs) Handles btnAjout.Click
+        Dim emp = New iFicheEmploye
+        emp.Owner = Me
+        emp.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
+        Me.Close()
+    End Sub
 End Class
