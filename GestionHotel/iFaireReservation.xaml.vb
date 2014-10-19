a8760754ff0f42eda02f3cf51635bd3b1b68a10f
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-10-19 14:48:48 -0400

    un bon ptit push maison

--- GestionHotel/iFaireReservation.xaml.vb
+++ GestionHotel/iFaireReservation.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Public Class iFaireReservation
     Private noEmpl As Short
     Private noHotel As Short
+    Dim typeChambre As Char
     Dim bd As P2014_Equipe2_GestionHôtelièreEntities
     Sub New(p1 As Short, p2 As Short)
         ' TODO: Complete member initialization 
@@ -11,6 +12,8 @@
     Private Sub window_FaireReserv_Loaded(sender As Object, e As RoutedEventArgs) Handles window_FaireReserv.Loaded
         bd = New P2014_Equipe2_GestionHôtelièreEntities
 
+        Dim res = From el In bd.tblTypeChambre Select el
+        cbTypeChambre.DataContext = res.ToList()
     End Sub
 
     Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
@@ -23,10 +26,21 @@
         chambre.dateFinSejour = dpDateFin.SelectedDate
         chambre.dateReserv = dpDateCreation.SelectedDate
         chambre.commentaire = txtCommReser.Text
+        chambre.noChambre = cbNoChambre.SelectedItem
+        chambre.noEmpl = noEmpl
+        chambre.noHotel = noHotel
 
 
         bd.tblReservationChambre.Add(chambre)
         bd.SaveChanges()
         MessageBox.Show("La réservation de la chambre a été fait avec succès.")
+    End Sub
+
+    Private Sub cbTypeChambre_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cbTypeChambre.SelectionChanged
+
+        typeChambre = cbTypeChambre.SelectedItem
+
+        Dim res2 = From le In bd.tblChambre Where noHotel = le.noHotel And le.codeTypeChambre = typeChambre Select le
+        cbNoChambre.DataContext = res2.ToList()
     End Sub
 End Class
