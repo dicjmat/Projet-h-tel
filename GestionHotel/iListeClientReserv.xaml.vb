93d8f62bae4b8d14fbe31356721266c1f49ecd5a
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-10-19 16:01:21 -0400

    besoin d'attention

--- GestionHotel/iListeClientReserv.xaml.vb
+++ GestionHotel/iListeClientReserv.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Public Class iListeClientReserv
     Private noEmpl As Short
     Private noHotel As Short
+    Dim login As New iLogin
     Dim reserv As iFaireReservation
     Sub New(p1 As Short, p2 As Short)
         ' TODO: Complete member initialization 
@@ -16,5 +17,10 @@
         reserv = New iFaireReservation(noEmpl, noHotel)
         reserv.Owner = Me
         reserv.Show()
+    End Sub
+
+    Private Sub btnDeco_Click(sender As Object, e As RoutedEventArgs) Handles btnDeco.Click
+        Me.Close()
+        login.Show()
     End Sub
 End Class
