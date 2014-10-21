69ed23065be8929fba00d4afb2e83bd8eac970d7
dicjmat <marc-ant1@hotmail.com>
2014-10-21 16:27:49 -0400

    je veux le stuff

--- GestionHotel/iAjoutForf.xaml.vb
+++ GestionHotel/iAjoutForf.xaml.vb
@@ -1,8 +1,26 @@
 ﻿Public Class iAjoutForf
     Dim bd As P2014_Equipe2_GestionHôtelièreEntities
+    Private ajout As Boolean
+
+    Sub New(p1 As Boolean)
+        ' TODO: Complete member initialization 
+        InitializeComponent()
+        ajout = p1
+    End Sub
 
     Private Sub windowAjoutForf_Loaded(sender As Object, e As RoutedEventArgs) Handles windowAjoutForf.Loaded
         bd = New P2014_Equipe2_GestionHôtelièreEntities
+        If ajout Then
+            btnModifierForf.IsEnabled = False
+            btnModifierForf.Visibility = Windows.Visibility.Hidden
+            btnAjouterForf.IsEnabled = True
+            btnAjouterForf.Visibility = Windows.Visibility.Visible
+        Else
+            btnModifierForf.IsEnabled = True
+            btnModifierForf.Visibility = Windows.Visibility.Visible
+            btnAjouterForf.IsEnabled = False
+            btnAjouterForf.Visibility = Windows.Visibility.Hidden
+        End If
 
         Dim res = From el In bd.tblTypeChambre Select el.nomTypeChambre
         cbTypeChambre.DataContext = res.ToList()
@@ -13,13 +31,15 @@
         Dim Forfait As New tblForfait
         Forfait.prixForfait = txtPrixForf.Text
         Forfait.descForfait = txtDescAct.Text
+        Forfait.codeTypeChambre = cbTypeChambre.SelectedItem.codeTypeChambre
+        If  Then
+
+        End If
+
+        Forfait.etatForfait
 
         bd.tblForfait.Add(Forfait)
         bd.SaveChanges()
         MessageBox.Show("Le forfait a été créé avec succès.")
-    End Sub
-
-    Private Sub lstTypeChambre_MouseDown(sender As Object, e As MouseButtonEventArgs)
-
     End Sub
 End Class
