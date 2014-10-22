aacbda8c3e0acb90ce06cd9dc01da2aebf5f8af0
dicjmat <marc-ant1@hotmail.com>
2014-10-22 09:39:42 -0400

    erreur de marde

--- GestionHotel/ListeForfait.xaml.vb
+++ GestionHotel/ListeForfait.xaml.vb
@@ -23,8 +23,10 @@
 
     Private Sub requete()
         Dim res = From fo In bd.tblForfait
-                  Where fo.noHotel = _hotel
-                  Select fo
+                  Join tych In bd.tblTypeChambre
+                  On tych.codeTypeChambre Equals fo.codeTypeChambre
+                  Where fo.noHotel = _hotel And (fo.nomForfait.StartsWith(txtrechercher.Text) Or tych.nomTypeChambre.StartsWith(txtrechercher.Text))
+                  Select fo.nomForfait, fo.noForfait, fo.etatForfait, tych.nomTypeChambre, fo.prixForfait
 
         dgForfait.ItemsSource = res.ToList
     End Sub
@@ -36,12 +38,22 @@
     End Sub
 
     Private Sub btnModifForf_Click(sender As Object, e As RoutedEventArgs) Handles btnModifForf.Click
-        Dim forfait = New iAjoutForf(False, _hotel)
-        forfait.Owner = Me
-        forfait.Show()
+        If dgForfait.SelectedIndex <> -1 Then
+            Dim forfait = New iAjoutForf(False, _hotel, dgForfait.SelectedItem.noForfait)
+            forfait.Owner = Me
+            forfait.Show()
+        Else
+            MessageBox.Show("Veuillez choisir un forfait")
+        End If
+
+
     End Sub
 
     Private Sub window_ListeForf_Activated(sender As Object, e As EventArgs) Handles window_ListeForf.Activated
+        requete()
+    End Sub
+
+    Private Sub txtrechercher_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtrechercher.TextChanged
         requete()
     End Sub
 End Class
