bad1d319abb9561b0e9fa5f64e5d8215ac2e8a9f
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-10-31 09:48:13 -0400

    push le en bas des marches!

--- GestionHotel/iAjoutCliReserv.xaml.vb
+++ GestionHotel/iAjoutCliReserv.xaml.vb
@@ -1,6 +1,21 @@
 ﻿Public Class iAjoutCliReserv
     Dim bd As P2014_Equipe2_GestionHôtelièreEntities
-
+    Sub New()
+        ' TODO: Complete member initialization 
+        InitializeComponent()
+    End Sub
+    Sub New(maBD As P2014_Equipe2_GestionHôtelièreEntities, _nomClient As String, _prenClient As String, _noTel As String, _noCell As String, _adr As String, _noCarte As String, _type As String, _dateEx As String)
+        ' TODO: Complete member initialization 
+        InitializeComponent()
+        bd = maBD
+        txtNomCli.Text = _nomClient
+        txtPrenCli.Text = _prenClient
+        txtTelCli.Text = _noTel
+        txtCelCli.Text = _noCell
+        txtAdrCli.Text = _adr
+        txtNoCarteCredit.Text = _noCarte
+        txtCodeExp.Text = _dateEx
+    End Sub
     Private Sub window_AjoutCliReserv_Loaded(sender As Object, e As RoutedEventArgs) Handles window_AjoutCliReserv.Loaded
         bd = New P2014_Equipe2_GestionHôtelièreEntities
         Dim res = From cli In bd.tblClient
