7190c3001bd93df69358126bc29fa3943c3e6cd3
David Savard <david.savard18@hotmail.com>
2014-11-03 15:03:06 -0500

    code menu recep v1

--- GestionHotel/iCheck_in-out.xaml.vb
+++ GestionHotel/iCheck_in-out.xaml.vb
@@ -1,3 +1,52 @@
 ﻿Public Class iCheck_in_out
 
+    Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
+        'Dim accueil = New iFaireReservation
+        'accueil.Owner = Me
+        'accueil.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnReservC_Click(sender As Object, e As RoutedEventArgs) Handles btnReservC.Click
+        'Dim reserv = New iFaireReservation(bd, noEmploye, noHotel)
+        'reserv.Owner = Me
+        'reserv.Show()
+    End Sub
+
+    Private Sub btnFact_Click(sender As Object, e As RoutedEventArgs) Handles btnFact.Click
+        Dim facture = New iFacture
+        facture.Owner = Me
+        facture.Show()
+    End Sub
+
+    Private Sub btnAjoutCli_Click(sender As Object, e As RoutedEventArgs) Handles btnAjoutCli.Click
+        Dim Cli = New iAjoutCliReserv
+        Cli.Owner = Me
+        Cli.Show()
+    End Sub
+
+    Private Sub btnFicheC_Click(sender As Object, e As RoutedEventArgs) Handles btnFicheC.Click
+        Dim fiche = New iFicheClient
+        fiche.Owner = Me
+        fiche.Show()
+    End Sub
+
+    Private Sub btnFicheReserv_Click(sender As Object, e As RoutedEventArgs) Handles btnFicheReserv.Click
+        Dim ficheR = New iFicheReserv
+        ficheR.Owner = Me
+        ficheR.Show()
+    End Sub
+
+    Private Sub btnFicheReservF_Click(sender As Object, e As RoutedEventArgs) Handles btnFicheReservF.Click
+        Dim ficheRF = New iFicheReservFacture
+        ficheRF.Owner = Me
+        ficheRF.Show()
+    End Sub
+
+    Private Sub btnListeCli_Click(sender As Object, e As RoutedEventArgs) Handles btnListeCli.Click
+        Dim lst = New iListeClient
+        lst.owner = Me
+        lst.Show()
+    End Sub
 End Class
+
