1625533c764d08182e3be1ce124442b598a128cc
dicjmat <marc-ant1@hotmail.com>
2014-11-26 09:36:53 -0500

    blbablslb

---
+++ GestionHotel/iAjouterCommunique.xaml.vb
@@ -0,0 +1,10 @@
+﻿Public Class iAjouterCommunique
+
+    Private Sub btnAjouter_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouter.Click
+
+    End Sub
+
+    Private Sub btnAnnuler_Click(sender As Object, e As RoutedEventArgs) Handles btnAnnuler.Click
+        Me.Close()
+    End Sub
+End Class
