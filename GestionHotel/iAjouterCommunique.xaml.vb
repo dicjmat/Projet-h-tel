40d2f5ce5d7c50909e8380450b43629375ab8af1
dicjmat <marc-ant1@hotmail.com>
2014-11-27 09:32:25 -0500

    Communiqué

--- GestionHotel/iAjouterCommunique.xaml.vb
+++ GestionHotel/iAjouterCommunique.xaml.vb
@@ -1,10 +1,71 @@
 ﻿Public Class iAjouterCommunique
 
+    Private maBd As P2014_Equipe2_GestionHôtelièreEntities
+    Private _noHotel As Short
+    Private noCommu As Integer
+
+    Sub New(_maBd As P2014_Equipe2_GestionHôtelièreEntities, noHotel As Short)
+        InitializeComponent()
+        maBd = _maBd
+        _noHotel = noHotel
+        btnModifier.Visibility = Windows.Visibility.Hidden
+    End Sub
+
+    Sub New(_maBd As P2014_Equipe2_GestionHôtelièreEntities, noHotel As Short, p3 As Integer)
+        InitializeComponent()
+        maBd = _maBd
+        _noHotel = noHotel
+        noCommu = p3
+        btnAjouter.Visibility = Windows.Visibility.Hidden
+        Dim commu = From co In maBd.tblCommunique Where _noHotel = co.noHotel Select co
+
+        txtNomCommunique.Text = commu.Single.titreCommunique
+        txtTexteCommunique.Text = commu.Single.contCommunique
+        If commu.Single.etatCommunique = "actif" Then
+            chkStatutCommunique.IsChecked = True
+        Else
+            chkStatutCommunique.IsChecked = False
+        End If
+    End Sub
+
     Private Sub btnAjouter_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouter.Click
+        If txtNomCommunique.Text <> "" And txtTexteCommunique.Text <> "" Then
+            Dim commu = New tblCommunique
+            commu.noHotel = _noHotel
+            commu.titreCommunique = txtNomCommunique.Text
+            commu.contCommunique = txtTexteCommunique.Text
+            If chkStatutCommunique.IsChecked Then
+                commu.etatCommunique = "actif"
+            Else
+                commu.etatCommunique = "inactif"
+            End If
+            maBd.tblCommunique.Add(commu)
+            maBd.SaveChanges()
+            MessageBox.Show("Le communiqué a bien été ajouté")
+            Me.Close()
+        End If
+
 
     End Sub
 
     Private Sub btnAnnuler_Click(sender As Object, e As RoutedEventArgs) Handles btnAnnuler.Click
         Me.Close()
+    End Sub
+
+    Private Sub btnModifier_Click(sender As Object, e As RoutedEventArgs) Handles btnModifier.Click
+        Dim commu = From co In maBd.tblCommunique Where _noHotel = co.noHotel Select co
+
+        If txtNomCommunique.Text <> "" And txtTexteCommunique.Text <> "" Then
+            commu.Single.titreCommunique = txtNomCommunique.Text
+            commu.Single.contCommunique = txtTexteCommunique.Text
+            If chkStatutCommunique.IsChecked Then
+                commu.Single.etatCommunique = "actif"
+            Else
+                commu.Single.etatCommunique = "inactif"
+            End If
+            maBd.SaveChanges()
+            MessageBox.Show("Le communiqué a bien été modifié")
+            Me.Close()
+        End If
     End Sub
 End Class
--- GestionHotel/iCommunique.xaml.vb
+++ GestionHotel/iCommunique.xaml.vb
@@ -34,4 +34,69 @@
         lst.Owner = Me
         lst.Show()
     End Sub
+
+    Private Sub txtRecherche_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtRecherche.TextChanged
+        requete()
+    End Sub
+
+    Private Sub requete()
+        Dim res = From el In maBd.tblCommunique
+                  Where _noHotel = el.noHotel And (el.titreCommunique.StartsWith(txtRecherche.Text) Or el.etatCommunique.StartsWith(txtRecherche.Text))
+                  Select el
+
+        dgCommuni.ItemsSource = res.ToList
+    End Sub
+
+    Private Sub Window_Activated(sender As Object, e As EventArgs)
+        requete()
+    End Sub
+
+    Private Sub dgCommuni_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles dgCommuni.SelectionChanged
+        If dgCommuni.SelectedIndex <> -1 Then
+            txtCommuni.Text = dgCommuni.SelectedItem.contCommunique
+        End If
+    End Sub
+
+    Private Sub btnAccueil_Click(sender As Object, e As RoutedEventArgs) Handles btnAccueil.Click
+        Me.Owner.Hide()
+        Me.Owner.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnSupprimerCommuni_Click(sender As Object, e As RoutedEventArgs) Handles btnSupprimerCommuni.Click
+        If dgCommuni.SelectedIndex <> 0 Then
+            Dim noCommu As Integer = dgCommuni.SelectedItem.noCommunique
+            Dim res = From co In maBd.tblCommunique
+                      Where co.noCommunique = noCommu
+                      Select co
+            maBd.tblCommunique.Remove(res.Single)
+            Dim maxi = From commu In maBd.tblCommunique
+                       Select commu.noCommunique
+            maBd.SaveChanges()
+            maBd.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Personnel.tblCommunique',RESEED," + maxi.ToList.Last.ToString + ")")
+            maBd.SaveChanges()
+            requete()
+            txtCommuni.Text = ""
+            MessageBox.Show("Le communiqué a bien été supprimé")
+        Else
+            MessageBox.Show("Veuillez choisir le communiqué que vous voulez supprimer")
+        End If
+    End Sub
+
+    Private Sub btnAjouterCommuni_Click(sender As Object, e As RoutedEventArgs) Handles btnAjouterCommuni.Click
+        Dim ajoutCommu = New iAjouterCommunique(maBd, _noHotel)
+        ajoutCommu.Owner = Me
+        ajoutCommu.ShowDialog()
+    End Sub
+
+    Private Sub btnModifierCommuni_Click(sender As Object, e As RoutedEventArgs) Handles btnModifierCommuni.Click
+        If dgCommuni.SelectedIndex <> -1 Then
+            Dim ajoutCommu = New iAjouterCommunique(maBd, _noHotel, dgCommuni.SelectedItem.noCommunique)
+            ajoutCommu.Owner = Me
+            ajoutCommu.ShowDialog()
+        Else
+            MessageBox.Show("Veuillez choisir le communiqué que vous désirez modifier")
+        End If
+
+    End Sub
 End Class
